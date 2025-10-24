9f5c965afbbed2472470ccab68ef5e5b1c698d07
Ajayi Ayanfe <ajayiayanfe9@gmail.com>
2025-10-24 14:29:43 +0100

    initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "4f31b6eace9f152e846b0fa89ed82ee3";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
